b94deedb4cf8c0bffb7b9bedb1773d6d6b72bf7d
Michelle Siregar <michellesiregar@gmail.com>
2019-10-28 23:08:29 +0000

    added urls_show to show the long url in reference to a short url

--- express_server.js
+++ express_server.js
@@ -8,6 +8,7 @@ app.listen(PORT, () => {
   console.log(`Tiny app listening on port ${PORT}`);
 });
 
+// object containing short and long urls
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
@@ -30,4 +31,12 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render('urls_show', templateVars);
 });
