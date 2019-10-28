d4f80ee8c2f30c837067c087b1df3d79b1f55392
Michelle Siregar <michellesiregar@gmail.com>
2019-10-28 21:36:43 +0000

    added urls_index view for list of urls

--- express_server.js
+++ express_server.js
@@ -4,17 +4,19 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+app.listen(PORT, () => {
+  console.log(`Tiny app listening on port ${PORT}`);
+});
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+
+// list of responses to url paths
 app.get('/', (req, res) => {
   res.send('Hello!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
 });
 
 app.get('/urls.json', (req, res) => {
@@ -23,4 +25,9 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
