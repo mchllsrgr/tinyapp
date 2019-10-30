c3b0ec9480ce4c96b60b9358c92804551fc441ee
Michelle Siregar <michellesiregar@gmail.com>
2019-10-30 00:38:37 +0000

    added users object to keep track of users

--- express_server.js
+++ express_server.js
@@ -30,6 +30,20 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+// object containing users
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 
 // list of responses to requests
 
