37ddee72ff1996f5c7198ef91690c2103cb3c0c2
Michelle Siregar <michellesiregar@gmail.com>
2019-10-31 20:15:03 +0000

    add mocha chai, create helper function test

---
+++ test/helpersTest.js
@@ -0,0 +1,23 @@
+const { assert } = require('chai');
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
+  },
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk'
+  }
+};
+
+describe('getUserByEmail', () => {
+  it('should return a user with valid email', () => {
+    const user = getUserByEmail('user2@example.com', testUsers);
+    const expectedOutput = 'user2RandomID';
+    assert.equal(user, expectedOutput);
+  });
+});
